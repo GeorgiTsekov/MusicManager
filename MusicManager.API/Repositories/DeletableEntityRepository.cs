c821c03df7cdfec8336231483e8f154bd5b56063
Georgi Tsekov <georgi_tsekov@abv.bg>
2023-09-18 00:34:54 +0300

    Add Repositories DeletableEntityRepository and EfRepository. Delete other repos.

---
+++ MusicManager.API/Repositories/DeletableEntityRepository.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MusicManager.API.Common.Models;
+using MusicManager.API.Common.Repositories;
+using MusicManager.API.Data;
+
+namespace MusicManager.API.Repositories
+{
+    public class DeletableEntityRepository<TEntity, TKey> : EfRepository<TEntity, TKey>, IDeletableEntityRepository<TEntity, TKey>
+        where TEntity : BaseDeletableModel<TKey>
+    {
+        public DeletableEntityRepository(MusicManagerDbContext db)
+            : base(db)
+        {
+        }
+
+        public virtual async Task<IList<TEntity>> AllAsync()
+        {
+            var entities = await this.DbSet.ToListAsync();
+            entities = entities.Where(x => !x.IsDeleted).ToList();
+            return entities;
+        }
+
+        public virtual async Task<IList<TEntity>> AllAsNoTrackingAsync()
+        {
+            var entities = await this.DbSet.AsNoTracking().ToListAsync();
+            entities = entities.Where(x => !x.IsDeleted).ToList();
+            return entities;
+        }
+
+        public async Task<IList<TEntity>> AllWithDeleted() => await this.DbSet.ToListAsync();
+
+        public async Task<IList<TEntity>> AllAsNoTrackingWithDeleted() => await this.DbSet.AsNoTracking().ToListAsync();
+
+        public void HardDelete(TEntity entity)
+        {
+            base.Delete(entity);
+        }
+
+        public void Undelete(TEntity entity)
+        {
+            entity.IsDeleted = false;
+            entity.DeletedOn = null;
+            base.Update(entity);
+        }
+
+        public override void Delete(TEntity entity)
+        {
+            entity.DeletedOn = DateTime.Now;
+            entity.IsDeleted = true;
+            base.Update(entity);
+        }
+    }
+}
