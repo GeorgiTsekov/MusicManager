5117323fa80282dacf22cb4d8b3f54bb9fe1741a
Georgi Tsekov <georgi_tsekov@abv.bg>
2023-09-18 00:33:32 +0300

    initial Common/Repositories with IdeletableER and IRepository

---
+++ MusicManager.API/Common/Repositories/IDeletableEntityRepository.cs
@@ -0,0 +1,13 @@
+﻿using MusicManager.API.Common.Models;
+
+namespace MusicManager.API.Common.Repositories
+{
+    public interface IDeletableEntityRepository<TEntity, TKey> : IRepository<TEntity, TKey>
+        where TEntity : BaseDeletableModel<TKey>
+    {
+        Task<IList<TEntity>> AllWithDeleted();
+        Task<IList<TEntity>> AllAsNoTrackingWithDeleted();
+        void HardDelete(TEntity entity);
+        void Undelete(TEntity entity);
+    }
+}
---
+++ MusicManager.API/Common/Repositories/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using MusicManager.API.Common.Models;
+
+namespace MusicManager.API.Common.Repositories
+{
+    public interface IRepository<TEntity, TKey>
+        where TEntity : BaseDeletableModel<TKey>
+    {
+        Task<IList<TEntity>> AllAsync();
+        Task<IList<TEntity>> AllAsNoTrackingAsync();
+        Task<TEntity> AddAsync(TEntity entity);
+        Task<TEntity> ByIdAsync(TKey id);
+        void Update(TEntity entity);
+        void Delete(TEntity entity);
+    }
+}
