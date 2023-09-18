d0dd43cfa4a3cbea727ebc5051fe4de8c3829a1d
Georgi Tsekov <georgi_tsekov@abv.bg>
2023-09-19 00:17:21 +0300

    Create Utils Folder

---
+++ MusicManager.API/Common/Utils/MMConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace MusicManager.API.Common.Utils
+{
+    public static class MMConstants
+    {
+        public const int DEFAULT_ENERGY = 100;
+        public const int UPDATED_ENERGY = 200;
+    }
+}
