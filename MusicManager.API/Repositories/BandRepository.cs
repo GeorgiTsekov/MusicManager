52db99b9ab5b9bb9f67321dcd1eae6b35401a5a8
Georgi Tsekov <georgi_tsekov@abv.bg>
2023-09-19 00:18:46 +0300

    BandRepository and MusicianRepository created

---
+++ MusicManager.API/Repositories/BandRepository.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MusicManager.API.Common.Repositories;
+using MusicManager.API.Data;
+using MusicManager.API.Models.Domain;
+
+namespace MusicManager.API.Repositories
+{
+    public class BandRepository : DeletableEntityRepository<Band, int>
+    {
+        public BandRepository(MusicManagerDbContext db) : base(db)
+        {
+        }
+    }
+}
---
+++ MusicManager.API/Repositories/MusicianRepository.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MusicManager.API.Common.Repositories;
+using MusicManager.API.Data;
+using MusicManager.API.Models.Domain;
+
+namespace MusicManager.API.Repositories
+{
+    public class MusicianRepository : DeletableEntityRepository<Musician, Guid>
+    {
+        public MusicianRepository(MusicManagerDbContext db) : base(db)
+        {
+        }
+
+        public async override Task<IList<Musician>> AllAsync()
+        {
+            return await base.DbSet.Where(x => !x.IsDeleted).Include(x => x.Band).ToListAsync();
+        }
+
+        public override async Task<Musician> ByIdAsync(Guid id)
+        {
+            return await base.DbSet.Include(x => x.Band).FirstOrDefaultAsync(x => x.Id == id && !x.IsDeleted);
+        }
+    }
+}
