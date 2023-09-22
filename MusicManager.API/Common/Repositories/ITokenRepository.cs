2209943da2fd40e0427ee71429fd617b3d6d64c4
Georgi Tsekov <georgi_tsekov@abv.bg>
2023-09-22 19:02:23 +0300

    Add TokenRepository, Jwt in appsettings.json and update Program.cs (AddIdentityCore, AddAuthentication, AddJwtBearer, Swagger Authentication...)

---
+++ MusicManager.API/Common/Repositories/ITokenRepository.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace MusicManager.API.Common.Repositories
+{
+    public interface ITokenRepository
+    {
+        string CreateJWTToken(IdentityUser user, List<string> roles);
+    }
+}
