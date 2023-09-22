1c2f3c265ad64ebe7694901b37352b3e66ee0064
Georgi Tsekov <georgi_tsekov@abv.bg>
2023-09-22 19:05:41 +0300

    DbSet Managers added.

--- MusicManager.API/Data/MusicManagerDbContext.cs
+++ MusicManager.API/Data/MusicManagerDbContext.cs
@@ -1,11 +1,13 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using MusicManager.API.Models.Domain;
 
 namespace MusicManager.API.Data
 {
-    public class MusicManagerDbContext : DbContext
+    public class MusicManagerDbContext : IdentityDbContext
     {
-        public MusicManagerDbContext(DbContextOptions<MusicManagerDbContext> dbContextOptions) : base(dbContextOptions)
+        public MusicManagerDbContext(DbContextOptions<MusicManagerDbContext> options) : base(options)
         {
         }
 
@@ -13,10 +15,35 @@ namespace MusicManager.API.Data
         public DbSet<Band> Bands { get; set; }
         public DbSet<Song> Songs { get; set; }
         public DbSet<Album> Albums { get; set; }
+        public DbSet<Manager> Managers { get; set; }
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        protected override void OnModelCreating(ModelBuilder builder)
         {
-            base.OnModelCreating(modelBuilder);
+            base.OnModelCreating(builder);
+
+            var readerRoleId = "5888d7e8-213b-46c5-a8f6-beca89e5a68a";
+            var writerRoleId = "5038b910-7a29-4163-ba89-c8a49a53e4d4";
+
+
+            var roles = new List<IdentityRole>
+            {
+                new IdentityRole
+                {
+                    Id = readerRoleId,
+                    ConcurrencyStamp = readerRoleId,
+                    Name = "Reader",
+                    NormalizedName = "Reader".ToUpper()
+                },
+                new IdentityRole
+                {
+                    Id = writerRoleId,
+                    ConcurrencyStamp = writerRoleId,
+                    Name = "Writer",
+                    NormalizedName = "Writer".ToUpper()
+                }
+            };
+
+            builder.Entity<IdentityRole>().HasData(roles);
         }
     }
 }
